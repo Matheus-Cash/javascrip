340f3693bc0c5e224d7bbaa9c2f69625e0548dc6
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-02-17 19:16:24 +0000

    124

--- templene-string.js
+++ templene-string.js
@@ -1,6 +1,6 @@
 const none = 'Palito157';
 const idade = 2025 - 1989;
-const cidadeDeNascimento ='cidadeDoGrau';
+const cidadeDeNascimento = 'cidadeDoGrau';
 
 const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci em ${cidadeNascimento};
 
