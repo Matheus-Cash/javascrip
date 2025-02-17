fddc6f906c6459d8c108a903a6df7fa2fdaf81e8
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-02-17 19:31:56 +0000

    214

--- comparacao2.js
+++ comparacao2.js
@@ -1,11 +1,10 @@
-const idadeMinima = 18
-const idadeCliente = 16
+const idadeMinima = 18;
+const idadeCliente = 16;
 
-
-if(idadeCliente >= idadeMinima){
-    console.log('cerveja')
+if (idadeCliente >= idadeMinima) {
+    console.log('cerveja');
 } else {
-    console.log('suco')
+    console.log('suco');
 }
 
-console.log(idadeCliente >=)
+console.log(idadeCliente >= idadeMinima);
